d9d18994d144537c42c0a8757c6ab925ea23b09b
anudeep22003 <anudeep.yegireddi@gmail.com>
2025-11-24 13:05:09 +0530

    feat: added basic authenticated user guard to PublicLayout

--- frontend/src/pages/auth/PublicLayout.tsx
+++ frontend/src/pages/auth/PublicLayout.tsx
@@ -1,6 +1,25 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
+import { useAuthContext } from "./context";
+import { AUTH_ROUTES } from "./constants";
+import { useEffect } from "react";
+import { authLogger } from "@/lib/logger";
 
 const PublicLayout = () => {
+  const { accessToken, checkingAuth } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    authLogger.debug("Checking Auth", { accessToken, checkingAuth });
+    if (!checkingAuth && accessToken) {
+      navigate(AUTH_ROUTES.HOME);
+    }
+  }, [accessToken, navigate, checkingAuth]);
+
+  // TODO: When logout is implemented, uncomment this
+  // if (checkingAuth) {
+  //   return <div>Checking Auth...</div>;
+  // }
+
   return <Outlet />;
 };
 
