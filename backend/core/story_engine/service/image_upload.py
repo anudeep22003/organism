2b9779d6478f88af6975bee699c38ab1134b3151
anudeep22003 <anudeep.yegireddi@gmail.com>
2026-03-19 01:41:10 +0000

    feat: variant creation and pillow work done (untested)

--- backend/core/story_engine/service/image_upload.py
+++ backend/core/story_engine/service/image_upload.py
@@ -1,7 +1,9 @@
+from dataclasses import dataclass
 from io import BytesIO
+from typing import BinaryIO
 
-from fastapi import UploadFile
 from google.cloud.storage import Client  # type: ignore[import-untyped]
+from PIL import Image, ImageOps
 
 from core.config import GCP_PROJECT_ID, GCP_STORAGE_BUCKET
 
@@ -13,6 +15,24 @@ from .dto_types import UploadReferenceImageDTO
 
 client = Client(project=GCP_PROJECT_ID)
 
+THUMB_SIZE = (100, 100)
+PREVIEW_SIZE = (500, 500)
+
+
+@dataclass
+class ReadyToUploadImageDTO:
+    width: int
+    height: int
+    size_bytes: int
+    image_bytes: BytesIO
+
+
+@dataclass
+class ImageVariants:
+    original: ReadyToUploadImageDTO
+    thumb: ReadyToUploadImageDTO
+    preview: ReadyToUploadImageDTO
+
 
 class ImageUploadService:
     def __init__(self, repository: Repository):
@@ -22,15 +42,18 @@ class ImageUploadService:
     async def upload_image(
         self,
         dto: UploadReferenceImageDTO,
-    ) -> None:
+    ) -> tuple[str, ImageVariants]:
         character = await self._get_authorized_character(dto)
         await self._create_in_processing_edit_event(dto)
-        _ = self._create_object_key(dto, character.slug)
+        object_key_prefix = self._create_object_key_prefix(dto, character.slug)
+        image_variants = await self._create_image_variants(dto.image)
 
         # next steps:
         # - create thumb, preview and original options using Pillow bufio
         # - upload to google storage
         # - update tables
+        # TODO continue with next steps
+        return object_key_prefix, image_variants
 
     async def _create_in_processing_edit_event(
         self, dto: UploadReferenceImageDTO
@@ -56,21 +79,49 @@ class ImageUploadService:
             )
         return character
 
-    def _create_object_key(
+    def _create_object_key_prefix(
         self, dto: UploadReferenceImageDTO, character_slug: str
     ) -> str:
-        return f"{dto.user_id}/character-references/{character_slug}"
+        return f"{dto.user_id}/character/{character_slug}/references/"
 
-    def _create_image_variants(self) -> dict[str, BytesIO]:
-        # create thumb, preview and original options using Pillow
-        # do we send byte array?
-        # cheap way to get size
-        # buf.seek(0, 2)     # move head to end
-        # size = buf.tell()  # head position == size
-        # buf.seek(0)        # rewind for upload
-        raise NotImplementedError("Not implemented")
+    async def _create_image_variants(self, file_byte_stream: BinaryIO) -> ImageVariants:
+        # read stream, get byte stream, create image and pass that forward
+        # following methods create a copy before running any operations
+        # To get size, you can buf.seek(2) and do a buf.tell() [2 is the end]
+        # reading a buf, automatically moves the pointer to the end so doing a tell on it gives you the size in bytes
+        # you could also look at length of the img_bytes
+        # creating BytesIO(img_bytes) create a new cursor wiht its own local pointer on the same img_bytes
+        img_bytes = file_byte_stream.read()
+        base_image = Image.open(BytesIO(img_bytes))
+        original = self._process_original_image(base_image)
+        thumb = self._create_thumb_image(base_image)
+        preview = self._create_preview_image(base_image)
+        return ImageVariants(original, thumb, preview)
 
-    def _upload_image_to_bucket(self, image: UploadFile) -> str:
+    def _pack_image_to_ready_to_upload_dto(
+        self, image: Image.Image, quality: int
+    ) -> ReadyToUploadImageDTO:
+        width, height = image.size
+        buffer = BytesIO()
+        image.save(buffer, format="JPEG", quality=quality, optimize=True)
+        size_bytes = buffer.tell()
+        return ReadyToUploadImageDTO(width, height, size_bytes, buffer)
+
+    def _process_original_image(self, image: Image.Image) -> ReadyToUploadImageDTO:
+        processed_img = ImageOps.exif_transpose(image=image.copy()).convert("RGB")
+        return self._pack_image_to_ready_to_upload_dto(processed_img, 90)
+
+    def _create_thumb_image(self, image: Image.Image) -> ReadyToUploadImageDTO:
+        thumb = image.copy()
+        thumb.thumbnail(THUMB_SIZE)  # modifies in place
+        return self._pack_image_to_ready_to_upload_dto(thumb, 90)
+
+    def _create_preview_image(self, image: Image.Image) -> ReadyToUploadImageDTO:
+        preview = image.copy()
+        preview.thumbnail(PREVIEW_SIZE)  # modifies in place
+        return self._pack_image_to_ready_to_upload_dto(preview, 90)
+
+    def _upload_image_to_bucket(self) -> str:
         raise NotImplementedError("Not implemented")
 
     async def _create_image_artefact(self) -> None:
