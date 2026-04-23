a90ec393d8f5df61f8a530236678b38b65aeebe6
anudeep22003 <anudeep.yegireddi@gmail.com>
2026-04-23 18:45:52 +0100

    refactor: remove legacy auth implementation

--- frontend/src/features/auth_legacy/model/auth.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { useAuth } from "./auth.context";
-
-export const useIsAuthenticated = () => {
-  return useAuth().status === "authenticated";
-};
-
-export const useIsCheckingAuth = () => {
-  return useAuth().status === "checking";
-};
