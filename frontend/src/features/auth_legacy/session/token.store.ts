be148453a6eedb435a491f26c6fc910c52137cf2
anudeep22003 <anudeep.yegireddi@gmail.com>
2026-04-23 18:45:52 +0100

    refactor: remove legacy auth implementation

--- frontend/src/features/auth_legacy/session/token.store.ts
+++
@@ -1,9 +0,0 @@
-import { httpClient } from "@/lib/httpClient";
-
-export const tokenStore = {
-  get: () => httpClient.getAccessToken(),
-  set: (token: string) => httpClient.setAccessToken(token),
-  clear: () => httpClient.clearSession(),
-  subscribe: (listener: (token: string | null) => void) =>
-    httpClient.subscribeToAccessToken(listener),
-};
